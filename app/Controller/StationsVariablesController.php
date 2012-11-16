2085e9130e6e0417ea4cc78c2713d614c2a7b974
Sean Cleveland <sean.b.cleveland@gmail.com>
2012-11-15 23:25:58 -0700

    adding a by_name for stations_variables

--- app/Controller/StationsVariablesController.php
+++ app/Controller/StationsVariablesController.php
@@ -29,7 +29,25 @@
                 '_serialize' => array('data')
             ));
         }
-
+         public function by_name($id) {   
+            $this->loadModel('Station');
+            $this->loadModel('Variable');
+              $results = $this->Station->findByName($id);
+              $variables =array();
+              $var = $this->StationsVariable->findAllByStationId($results['Station']['id']);
+              foreach($var as &$value){
+                //echo $value['StationsVariable']['variable_id'];
+                //echo ", ";
+                $variable = $this->Variable->findById($value['StationsVariable']['variable_id']);
+                array_push($variables, $variable['Variable']);
+              }
+              //print_r($variables);
+              $results['Variable'] =$variables;
+              $this->set(array(
+                  'data' => $results,
+                  '_serialize' => array('data')
+              ));
+          }
 
 
   }
