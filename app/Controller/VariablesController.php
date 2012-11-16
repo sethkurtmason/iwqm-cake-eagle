c8acdb87426616efe83d3bf444c2dd1729c16f1f
Sean Cleveland <sean.b.cleveland@gmail.com>
2012-11-15 20:24:17 -0700

    updated variable controller

--- app/Controller/VariablesController.php
+++ app/Controller/VariablesController.php
@@ -1,5 +1,20 @@
 <?php
   class VariablesController extends AppController {
-    public $scaffold;
+    public $components = array('RequestHandler');
+
+        public function index() {
+                $variables = $this->Variable->find('all');
+                $this->set(array(
+                    'variables' => $variables,
+                    '_serialize' => array('variables')
+                ));
+            }
+            public function view($id) {
+                $variable = $this->Variable->findById($id);
+                $this->set(array(
+                    'variable' => $variable,
+                    '_serialize' => array('variable')
+                ));
+            }
   }
 ?>
