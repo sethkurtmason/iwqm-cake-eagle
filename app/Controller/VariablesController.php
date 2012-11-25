0874e95621ff300a2a1eb4a9c377d8fb69328485
Sean Cleveland <sean.b.cleveland@gmail.com>
2012-11-24 20:31:55 -0700

    added unique_characteristic API call to fetch all unique characteristic names in the DB

--- app/Controller/VariablesController.php
+++ app/Controller/VariablesController.php
@@ -9,12 +9,23 @@
                     '_serialize' => array('variables')
                 ));
             }
-            public function view($id) {
-                $variable = $this->Variable->findById($id);
-                $this->set(array(
-                    'variable' => $variable,
-                    '_serialize' => array('variable')
-                ));
-            }
+        public function view($id) {
+            $variable = $this->Variable->findById($id);
+            $this->set(array(
+                'variable' => $variable,
+                '_serialize' => array('variable')
+            ));
+        }
+        public function unique_characteristics(){
+          $variables = $this->Variable->find('all',
+            array(
+              'fields' => array('DISTINCT Variable.term')
+            )
+          );
+          $this->set(array(
+              'variables' => $variables,
+              '_serialize' => array('variables')
+          ));
+        }
   }
 ?>
