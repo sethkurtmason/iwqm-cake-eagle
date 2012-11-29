239d7857a27194e76a96ffa31e3380a12114537d
Sean Cleveland <sean.b.cleveland@gmail.com>
2012-11-29 09:11:35 -0700

    made the characteristics_by_station_name API faster

--- app/Controller/StationsVariablesController.php
+++ app/Controller/StationsVariablesController.php
@@ -55,22 +55,33 @@
         $variable_array =array();
         $variables =array();
         $stations_array = $this->params['url']['stations'];
+        $station_ids=array();
         foreach ($stations_array as &$station_name) {
             $station = $this->Station->findByName($station_name);
-            $vars = $this->StationsVariable->findAllByStationId($station['Station']['id']);
-            foreach($vars as &$var){
-              $variable = $this->Variable->findById($var['StationsVariable']['variable_id'],
-                array(
-                  'fields'=>('Variable.term')
-                )
-              );
-              array_push($variable_array, $variable['Variable']['term']);
-            }
-        } 
-        $variable_array = array_unique($variable_array);
-        foreach($variable_array as $var){
-          array_push($variables, $var);
+            array_push($station_ids, $station['Station']['id']);
         }
+        $vars = $this->StationsVariable->findAllByStationId($station_ids);
+        foreach($vars as &$var){
+          array_push($variable_array,$var['StationsVariable']['variable_id']);
+        }
+        // $variable = $this->Variable->findAllById($variable_array,
+        //     array(
+        //       'fields'=>('Variable.term')
+        //     )
+        //   );
+        $ids = implode(",",$variable_array);
+        $q = "select DISTINCT term from variables where id in ($ids)";
+        $db = ConnectionManager::getDataSource('default');
+        //$results = $db->rawQuery($q);
+        $results = $this->Variable->query($q);
+        //print_r($results);
+        foreach($results as &$var){
+          array_push($variables,$var[0]["term"]);
+        }
+        //$variables= array_unique($variable_array);
+        // foreach($variable_array as $var){
+        //   array_push($variables, $var);
+        // }
         $this->set(array(
             'characteristics' => $variables,
             '_serialize' => array('characteristics')
